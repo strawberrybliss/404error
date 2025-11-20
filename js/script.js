3670e0d81be423223e2f08285e9748998d8d2cf5
sarahbzm22 <sbzm22abril@gmail.com>
2025-11-20 18:44:56 +0100

    cambios 20/11 (Terminado)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Barra de carga
+
 window.addEventListener("load", () => {
 
     const barra = document.getElementById("barra-carga");
@@ -7,48 +9,45 @@ window.addEventListener("load", () => {
     const icono = document.getElementById("alarma-icon");
     const pantallaFinal = document.getElementById("final");
 
-    // 1) Barra hasta el 80%
     setTimeout(() => {
         barra.style.width = "80%";
     }, 400);
 
-    // 2) Activar alarma después de la barra
+    // Activación de la alarma tras la carga de la barra
+
     setTimeout(() => {
 
-        // Mostrar contenedor de alarma
         alarma.style.opacity = 1;
-
-        // Iniciar animación de parpadeos
         flash.classList.add("flash-activo");
 
-        // → 5.2s coincide con el último parpadeo fuerte
         setTimeout(() => {
 
-            // A) La pantalla de carga desaparece justo cuando el flash está en opacidad 1
+            // Desaparición de la pantalla de carga
+
             carga.classList.add("fade-out");
 
-            // B) Flash y icono se desvanecen juntos con transición suave
+            // Desaparición del icono de alarma y luz roja
+
             flash.style.transition = "opacity 0.9s ease";
             icono.style.transition = "opacity 0.9s ease";
             flash.style.opacity = 0;
             icono.style.opacity = 0;
 
-            // C) Mostrar pantalla final suavemente después del fade
+            // Mostrar la pantalla final
+
             setTimeout(() => {
 
-                // --- CAMBIO IMPORTANTE ---
-                pantallaFinal.style.display = "flex"; // <<< NECESARIO PARA QUE APAREZCA
+                pantallaFinal.style.display = "flex";
 
                 pantallaFinal.style.opacity = 1;
                 pantallaFinal.style.pointerEvents = "auto";
 
-                // Limpiar elementos anteriores
                 carga.remove();
                 alarma.remove();
 
-            }, 1400); // coincide con la duración del fade
+            }, 1400);
 
-        }, 3000); // momento exacto del último flash
+        }, 3000);
 
-    }, 2800); // tiempo después de iniciar barra de carga
+    }, 2800);
 });
